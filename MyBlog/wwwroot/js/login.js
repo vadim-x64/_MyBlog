c4f817ba8837d5bbcd563f333dddcfe3f8c3671e
Vadym <vadim.rolex.2005@gmail.com>
2025-05-13 21:49:42 +0300

    Login page was fixed and completed as well.

---
+++ MyBlog/wwwroot/js/login.js
@@ -0,0 +1,13 @@
+﻿document.addEventListener('DOMContentLoaded', function() {
+    var alerts = document.querySelectorAll('.alert-dismissible');
+    alerts.forEach(function(alert) {
+        new bootstrap.Alert(alert);
+    });
+
+    const passwordInput = document.getElementById('passwordInput');
+    const showPasswordCheckbox = document.getElementById('showPasswordCheckbox');
+
+    showPasswordCheckbox.addEventListener('change', function() {
+        passwordInput.type = this.checked ? 'text' : 'password';
+    });
+});
