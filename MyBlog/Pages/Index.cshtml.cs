b0a4bdd1735c75bb511490c98051ddf3fd5a717c
Vadym <vadim.rolex.2005@gmail.com>
2025-05-28 21:05:58 +0300

    Fixed likes logic for posts here.

--- MyBlog/Pages/Index.cshtml.cs
+++ MyBlog/Pages/Index.cshtml.cs
@@ -1,41 +1,41 @@
-    using Microsoft.AspNetCore.Mvc;
-    using Microsoft.AspNetCore.Mvc.RazorPages;
-    using MyBlog.Models;
-    using MyBlog.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using MyBlog.Models;
+using MyBlog.Services;
 
-    namespace MyBlog.Pages;
+namespace MyBlog.Pages;
 
-    public class IndexModel : PageModel
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+    private readonly PostService _postService;
+    private readonly LikeService _likeService;
+
+    public List<Post> Posts { get; set; } = new List<Post>();
+    public Dictionary<Guid, bool> LikedPosts { get; set; } = new Dictionary<Guid, bool>();
+    public Dictionary<Guid, int> LikesCount { get; set; } = new Dictionary<Guid, int>();
+
+    public IndexModel(ILogger<IndexModel> logger, PostService postService, LikeService likeService)
     {
-        private readonly ILogger<IndexModel> _logger;
-        private readonly PostService _postService;
-        private readonly LikeService _likeService;
+        _logger = logger;
+        _postService = postService;
+        _likeService = likeService;
+    }
 
-        public List<Post> Posts { get; set; } = new List<Post>();
-        public Dictionary<Guid, bool> LikedPosts { get; set; } = new Dictionary<Guid, bool>();
-        public Dictionary<Guid, int> LikesCount { get; set; } = new Dictionary<Guid, int>();
+    public async Task OnGetAsync()
+    {
+        Posts = await _postService.GetAllPostsAsync();
 
-        public IndexModel(ILogger<IndexModel> logger, PostService postService, LikeService likeService)
+        foreach (var post in Posts)
         {
-            _logger = logger;
-            _postService = postService;
-            _likeService = likeService;
-        }
-
-        public async Task OnGetAsync()
-        {
-            Posts = await _postService.GetAllPostsAsync();
-            
-            foreach (var post in Posts)
-            {
-                LikedPosts[post.Id] = await _likeService.IsPostLikedByCurrentUserAsync(post.Id);
-                LikesCount[post.Id] = await _likeService.GetLikesCountAsync(post.Id);
-            }
-        }
-
-        public async Task<IActionResult> OnPostToggleLikeAsync(Guid postId)
-        {
-            await _likeService.ToggleLikeAsync(postId);
-            return RedirectToPage();
+            LikedPosts[post.Id] = await _likeService.IsPostLikedByCurrentUserAsync(post.Id);
+            LikesCount[post.Id] = await _likeService.GetLikesCountAsync(post.Id);
         }
     }
+
+    public async Task<IActionResult> OnPostToggleLikeAsync(Guid postId)
+    {
+        await _likeService.ToggleLikeAsync(postId);
+        return RedirectToPage();
+    }
+}
